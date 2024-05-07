281efc9f399cf94bce7552560110182a1ced7442
¨ZukaAC¨ <joao.oliveira@academiadecodigo.org>
2024-05-07 19:32:06 +0100

    added more characters

--- OnePiece/src/main/java/projects/zuka/persistence/Data.java
+++ OnePiece/src/main/java/projects/zuka/persistence/Data.java
@@ -19,32 +19,16 @@ public class Data {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
+    
     public String getImage() {
         return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
     }
 
 
